7fe7d422d18307d0c01c90c14b7ec8daf37d7bfd
Lajcee <lajceemillerdesign@gmail.com>
2020-10-13 15:20:49 +1300

    arrays

---
+++ js/script.js
@@ -0,0 +1,31 @@
+//***********************************
+//Testing my script if it is linked
+//***********************************
+
+console.log('js');
+
+
+$(document).ready(function(){
+
+
+});
+
+
+//declaration
+var username = ["Thomas", "James"];
+var age = [45,12];
+var hasLicense = [true, false];
+var i = 0;
+
+//implementation
+for (i = 0; i < 2; i++) {
+  // username[i] = prompt('Enter the name: ');
+  // age[i] = prompt('Enter  the age: ');
+  // hasLicense[i] = prompt('Enter  if the person has license or not: ');
+
+  // console.log(username[i], age[i],hasLicense[i]);
+  //
+  // document.getElementById('result').innerHTML += username[i] + '<br>' + age[i] + '<br>' + hasLicense[i] + '<br>' + '********' + '<br>';
+
+
+}
